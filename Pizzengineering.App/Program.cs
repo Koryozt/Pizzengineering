f889cc208a65fa1c49eefec0cb9424f09c354e9f
Gustavo Silva <gustavoasc2004@gmail.com>
2023-04-29 16:45:14 -0400

    Modifed a little bit the Blazor app.

--- Pizzengineering.App/Program.cs
+++ Pizzengineering.App/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Pizzengineering.App.Data;
 
 internal class Program
 {
@@ -11,7 +10,6 @@ internal class Program
 		// Add services to the container.
 		builder.Services.AddRazorPages();
 		builder.Services.AddServerSideBlazor();
-		builder.Services.AddSingleton<WeatherForecastService>();
 
 		var app = builder.Build();
 
